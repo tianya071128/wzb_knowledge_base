b249a0dd9c3c841e5d89072840ffafdcf9c8f0f5
--get <wenzb@esa2000.com>
2025-06-05 20:04:59 +0800

    ✨ feat: node02

--- demo/node/01模块/src/04_fs.ts
+++ demo/node/01模块/src/04_fs.ts
@@ -8,6 +8,8 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// #region ------------ 文件操作 ------------
+
 // #region ------------ 读取文件: readFile ------------
 try {
   // const content = await fsPromise.readFile(
@@ -94,7 +96,7 @@ try {
 
 // #endregion
 
-// #region ------------ 重命名文件: rename ------------
+// #region ------------ 重命名文件(或目录): rename ------------
 try {
   /**
    * 重命名文件或目录
@@ -113,18 +115,141 @@ try {
 // #endregion
 
 // #region ------------ 复制文件: copyFile ------------
-try {
+// try {
+//   /**
+//    * 复制文件
+//    *  1. 默认情况下，如果 dest 已经存在，则会被覆盖 --> 可通过 mode 参数更改行为
+//    *  2. 只支持文件, 不支持目录
+//    *  3. 如果 src 不存在, 则抛出异常
+//    */
+//   await fsPromise.copyFile(
+//     join(__dirname, '../public/test2.json'),
+//     join(__dirname, '../public/test_copy.json')
+//   );
+// } catch (e) {
+//   console.error('复制文件异常: ', e);
+// }
+// #endregion
+
+// #endregion
+
+// #region ------------ 目录操作 ------------
+
+// #region ------------ 创建目录: mkdir ------------
+// try {
+//   /**
+//    * 1. 默认情况下, 如果 path 是已存在的目录时, 抛出异常
+//    * 2. 当 recursive 设置为 true 时, 即使 path 是已存在的目录, 也不会抛出异常
+//    */
+//   await fsPromise.mkdir(path.join(__dirname, '../public2'), {
+//     recursive: true,
+//   });
+// } catch (e) {
+//   console.error('创建目录异常: ', e);
+// }
+
+// #endregion
+
+// #region ------------ 读取目录: readdir ------------
+// try {
+//   /**
+//    * 1. 默认情况下, 返回的是文件名称数组
+//    * 2. 设置 withFileTypes 选项, 返回的是 fs.Dirent 对象数组
+//    */
+//   const res = await fsPromise.readdir(path.join(__dirname, '../public'), {
+//     withFileTypes: true,
+//   });
+//   console.log(res); // [ 'newText.txt', 'test.json', 'test_copy.json' ]
+// } catch (e) {
+//   console.error('读取目录失败:', e);
+// }
+// #endregion
+
+// #region ------------ 删除目录: rmdir、rm ------------
+// try {
+//   /**
+//    * 1. 如果 path 不是目录, 抛出异常
+//    * 2. 默认情况下, 只会删除空目录, 可通过 recursive 选项配置 --> 已弃用, 所以还会删除，但会抛出警告，提示使用 rm 删除
+//    */
+//   await fsPromise.rmdir(path.join(__dirname, '../public/test'), {
+//     recursive: true,
+//   });
+// } catch (e) {
+//   console.error('删除目录异常', e);
+// }
+// #endregion
+
+// #region ------------ 复制目录(或文件): cp ------------
+// try {
+//   /**
+//    * 1. 当 path 为目录时, 则必须设置 recursive: true, 否则报错
+//    */
+//   await fsPromise.cp(
+//     path.join(__dirname, '../public/test'),
+//     path.join(__dirname, '../public/test2'),
+//     {
+//       recursive: true,
+//     }
+//   );
+// } catch (e) {
+//   console.error('复制目录(或文件)异常: ', e);
+// }
+
+// #endregion
+
+// #endregion
+
+// #region ------------ 其他操作 ------------
+{
   /**
-   * 复制文件
-   *  1. 默认情况下，如果 dest 已经存在，则会被覆盖 --> 可通过 mode 参数更改行为
-   *  2. 只支持文件, 不支持目录
-   *  3. 如果 src 不存在, 则抛出异常
+   * 返回文件(或目录)信息: stat(path[, options])
+   *
+   *  1. 同时支持文件和目录以及其他(链接等)
+   *  2. 如果只是检测文件或目录的权限(是否存在以及读写权限), 可通过 access 方法
    */
-  await fsPromise.copyFile(
-    join(__dirname, '../public/test2.json'),
-    join(__dirname, '../public/test_copy.json')
-  );
-} catch (e) {
-  console.error('复制文件异常: ', e);
+  // try {
+  //   const res = await fsPromise.stat(join(__dirname, '../public'));
+  //   // 返回 <fs.Stats> 对象
+  //   console.log(res);
+  // } catch (e) {
+  //   console.error('', e);
+  // }
 }
+// #endregion
+
+// #region ------------ FileHandle 类  ------------
+/**
+ * 1. FileHandle 是数字文件描述符的对象封装。用于操作该文件
+ * 2. 通过 fsPromises.open() 方法创建。
+ * 3. 如果未使用 filehandle.close() 方法关闭 <FileHandle>，则它将尝试自动关闭文件描述符并触发进程警告，从而有助于防止内存泄漏。请不要依赖此行为，因为它可能不可靠并且该文件可能未被关闭。
+ * 4. 应该始终显式关闭 <FileHandle>。
+ */
+
+// try {
+//   /**
+//    * open(path, flags[, mode]): 打开 <FileHandle>。
+//    *
+//    *  1. 不支持文件
+//    *  2. 默认情况下, 如果文件不存在, 则会报错 --> 可通过 flags 参数更改行为
+//    */
+//   const filehandle = await fsPromise.open(
+//     join(__dirname, '../public/test.json')
+//   );
+//   console.log(filehandle);
+
+//   /**
+//    * 分块读取文件: filehandle.read()
+//    */
+//   const buffer = Buffer.alloc(100); // 缓冲区
+//   await filehandle.read(buffer, 0, buffer.length, 0);
+//   console.log('读取的内容', buffer, buffer.toString());
+
+//   /**
+//    * 最后关闭文件文件句柄
+//    */
+//   filehandle.close();
+// } catch (e) {
+//   console.error('打开文件操作过程中异常', e);
+// }
+
 // #endregion
